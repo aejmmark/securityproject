66fbdbc01b6241e1198a4617dbd0ecc81de2e3c3
taikamake <aejmelaeus.markus@gmail.com>
2024-10-08 21:16:44 +0300

    Made logs available to all logged users

--- src/pages/views.py
+++ src/pages/views.py
@@ -19,7 +19,7 @@ def deleteNoteView(request):
 	n = Note.objects.get(pk=request.POST.get('id'))
 	if n.user_id==request.user.id:
 		n.delete()
-		logAction(n.user_id, str(n.user_id) + " deleted '" + n.text + "'")
+		#logAction(n.user_id, str(n.user_id) + " deleted '" + n.text + "'")
 	return redirect('/')
 
 @login_required
@@ -29,8 +29,13 @@ def addNoteView(request):
 	query = "INSERT INTO pages_note (user_id, text) VALUES (" + str(id) + ",'" + text + "')"
 	print(query)
 	connection.cursor().execute(query)
-	logAction(id, str(id) + " added '" + text + "'")
+	#logAction(id, str(id) + " added '" + text + "'")
 	return redirect('/')
+	#	text = request.POST.get('note')
+	#	n = Note(user_id=request.user.id, text=text)
+	#	n.save()
+	#logAction(id, str(id) + " added '" + text + "'")
+	#	return redirect('/')
 
 @login_required
 def homePageView(request):
@@ -39,33 +44,14 @@ def homePageView(request):
 
 @login_required
 def logView(request):
+	#if request.user.is_superuser:
+	#	logs = Log.objects.all()
+	#	return render(request, 'pages/logs.html', {'logs': logs})
+	#else:
+	#	return redirect('/')
 	logs = Log.objects.all()
 	return render(request, 'pages/logs.html', {'logs': logs})
 
 def logAction(id, action):
 	log = Log(user_id=id, action=action)
 	log.save()
-
-#@login_required
-#def deleteNoteView(request):
-#	n = Note.objects.get(pk=request.POST.get('id'))
-#	if n.user_id==request.user:
-#		n.delete()
-#	return redirect('/')
-#
-#@login_required
-#def addNoteView(request):
-#	text = request.POST.get('note')
-#	n = Note(user_id=request.user.id, text=text)
-#	n.save()
-#	return redirect('/')
-#
-#@login_required
-#def homePageView(request):
-#	notes = Note.objects.filter(user_id=request.user.id)
-#	return render(request, 'pages/index.html', {'notes': notes})
-
-#conn = sqlite3.connect(name)
-#agents = read_database(conn)
-#result = conn.execute("SELECT id, name FROM Agent ORDER BY id")
-#agents = result.fetchall()
